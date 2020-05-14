a1ac4ee2555410eff26cc20f5489b3041ce6b0af
mge <mge@pixum.com>
2020-05-14 22:32:48 +0200

    Update index.11ty.js

--- index.11ty.js
+++ index.11ty.js
@@ -16,25 +16,28 @@ class Test {
 
   render({name}) {
     // will always be "Ted"
-    return `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>${today.toString()}</title></head>
-<style>
-body {
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-  font-size: clamp(16px, calc(1rem + 0.5vw), 24px)
-}
-
-
-.arvelie{display: grid; grid-gap: 10px; padding: 10px; marign: 10px auto; justify-content: center;text-align: center}
-</style>
-
-    <body>
+    return `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${today.toString()}</title>
+    <style>
+    body {
+      font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+      font-size: clamp(16px, calc(1rem + 0.5vw), 24px)
+    }
+    .arvelie{display: grid; grid-gap: 10px; padding: 10px; marign: 10px auto; justify-content: center;text-align: center}
+    </style>
+  </head>
+  <body>
     <div class="arvelie">
       <span class="y">${today.y}</span>
       <span class="m">${today.m}</span>
       <span class="d">${today.d}</span>
     </div>
-    </body>
-    </html>`
+  </body>
+</html>`
   }
 }
 
